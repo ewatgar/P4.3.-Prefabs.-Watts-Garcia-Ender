ddd59c7a875261ae17c85b6fc04f8c6a743f0df0
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-01-23 20:15:27 +0100

    texturas + restringir rotacion cuello

--- Assets/Scripts/RotadorCabeza.cs
+++ Assets/Scripts/RotadorCabeza.cs
@@ -11,6 +11,19 @@ public class RotadorCabeza : MonoBehaviour
 
     void Update()
     {
-        transform.LookAt(target.transform);
+        Vector3 direccionPersonaje = transform.parent.forward;
+        Vector3 direccionFirstPerson = (target.transform.position - transform.position).normalized;
+        float prodEscalarCampoVision = Vector3.Dot(direccionPersonaje,direccionFirstPerson);
+        if (prodEscalarCampoVision > 0.3f){
+            transform.LookAt(target.transform);
+        } else{
+            transform.localEulerAngles = new Vector3(direccionPersonaje.x,0,0);
+            /*
+            Quaternion startRotation = Quaternion.LookRotation(direccionFirstPerson);
+            Quaternion endRotation = Quaternion.LookRotation(direccionPersonaje);
+            Quaternion smoothAnimation = Quaternion.Slerp(startRotation,endRotation,0.5f);
+            transform.rotation = smoothAnimation;*/
+        }
+
     }
 }
