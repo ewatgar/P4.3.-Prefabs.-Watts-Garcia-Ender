e31a8c326ac1322973e207164b85764ec1d09cef
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-01-23 19:34:38 +0100

    Animacion extremidades

---
+++ Assets/Scripts/RotadorExtremidades.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotadorExtremidades : MonoBehaviour
+{
+    [SerializeField] public float angMinimo = -30;
+    [SerializeField] public float angMaximo = 30;
+    [SerializeField] public float vAngular = 150;
+    [SerializeField] public float direccion = 1;
+
+    private float anguloTotal = 0;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (anguloTotal >= angMaximo || anguloTotal <= angMinimo){
+            direccion *= -1;
+            anguloTotal = Mathf.Clamp(anguloTotal,angMinimo,angMaximo);
+        }
+
+        float angulo = vAngular * Time.deltaTime;
+        anguloTotal += direccion*angulo;
+        
+        transform.localEulerAngles = new Vector3(anguloTotal,0,0);
+    }
+}
